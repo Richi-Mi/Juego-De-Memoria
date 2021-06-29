b49f068d4fe30e0feea33b0b23dbaf5e8ab22a53
Jose Ricardo Mendoza Castañeda <mjoseanakin@gmail.com>
2021-06-28 22:04:54 -0500

    Terminamos la funcion que ordena el array aleatoriamente y dimos unos retoques al codigo, ademas de sentar las bases para lo final

---
+++ cardTo.js
@@ -0,0 +1,8 @@
+class CardTo {
+    setFirstCard(card1) {
+        this.firstCard = card1 
+    }
+    setSecondCard(card2) {
+        this.secondCard = card2
+    }
+}
--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const tablero = document.getElementById('tablero')
 const fragment = document.createDocumentFragment()
-const textCards = ['Frontend', 'Backend', 'JavaScript', 'CSS', 'HTML', 'NodeJS', 'MongoDB', 'MySQL', 'Frontend', 'Backend', 'JavaScript', 'CSS', 'HTML', 'NodeJS', 'MongoDB', 'MySQL']
+let textCards = ['Frontend', 'Backend', 'JavaScript', 'CSS', 'HTML', 'NodeJS', 'MongoDB', 'MySQL', 'Frontend', 'Backend', 'JavaScript', 'CSS', 'HTML', 'NodeJS', 'MongoDB', 'MySQL']
+
+//Funciones
+
+textCards = textCards.sort(() => { return Math.random() - 0.5})
 
 textCards.forEach( text => {
     const divCard = document.createElement('div')
@@ -27,17 +31,17 @@ textCards.forEach( text => {
 } )
 tablero.appendChild(fragment)
 
-let boolean   = false
-
-const reveal = (e) => {
+const revealCard = (e) => {
     let cardBack = e.target
     let cardFront = e.target.parentElement.children.front
 
     cardBack.classList.add('active')
 
     setTimeout(() => cardFront.classList.add('active'), 200)
+
+    return cardFront.children[0].textContent
 }
-const hidden = (e) => {
+const hiddenCard = (e) => {
     let cardFront = e.target
     let cardBack  = e.target.parentElement.children.back
 
@@ -45,21 +49,25 @@ const hidden = (e) => {
 
     setTimeout(() => cardBack.classList.remove('active'), 200)
 }
+
+// Evento del Teclado 
+
 tablero.addEventListener('click', (e) => {
     try {
         let status = e.target.parentElement.attributes.status.value
 
         if(status === 'hidden') {
-            reveal(e)
+            const card = revealCard(e)
             e.target.parentElement.attributes.status.value = 'visible'
         }
         else if(status === 'visible') {
-            hidden(e)
+            hiddenCard(e)
             e.target.parentElement.attributes.status.value = 'hidden'
         }
         else {
             throw new Error('Alguien modifico un dato')
         }
     } catch (error) {
+        console.log(error)
     }
 })
